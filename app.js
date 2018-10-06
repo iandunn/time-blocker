f11af41437e8c6a6810fec1e6bad728ded91dfab
Ian Dunn <ian@iandunn.name>
2018-10-06 13:06:22 -0700

    initial draft

---
+++ app.js
@@ -0,0 +1,27 @@
+// look into web components
+// try to find a modern way that doesn't require a fraking build step
+	// maybe that "HEC/HEX" article on medium
+		// es6 is mostly supported natively by browsers, and has template literals
+
+( function() {
+	'use strict';
+
+	let timeBlocks = {};
+
+	const app = {
+		// jsdoc
+		init : function() {
+			// const template = _.template( 'time-block-row', timeBlocks );
+			// document.getElementsByClassName(  )
+		},
+
+		saveTemplate : function() {
+		},
+
+		loadTemplate : function() {
+			// warn that will override current blocks
+		}
+	};
+
+	app.init();
+}() );
